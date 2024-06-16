3a124967f2d5873d9a79de4becf323f49246ff1d
Kaspeodor <marcusvini16051998@gmail.com>
2024-06-16 19:52:19 -0300

    alert de erros de valores

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const button = document.querySelector(".generateButton")
 function generateNumber() {
   const min = Math.ceil(document.querySelector (".inputEntre").value);
   const max = Math.floor(document.querySelector (".inputE").value);
-  const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  if (min >= max){
+    alert ('O valor do "entre" deve ser inferior ao segundo valor e ambos não podem ser iguais (value Error.404)')
+  }
+  else {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
   const displayResult = document.querySelector(".randomResult")
   displayResult.style.visibility = "visible"
   displayResult.innerHTML = result
+  }
+  
 }
 
 button.addEventListener ('click', generateNumber)
